66863731cd4f9c64170c615f4a8b32d3dd5ee085
Technote <technote.space@gmail.com>
2019-09-29 04:11:59 +0900

    fix: codefactor issues

--- bin/gh-pages/pre_setup.sh
+++ bin/gh-pages/pre_setup.sh
@@ -12,8 +12,8 @@ pwd)
 SCRIPT_DIR=${1}
 source ${SCRIPT_DIR}/variables.sh
 
-VERSION=$(cat ${TRAVIS_BUILD_DIR}/package.json | jq -r .version)
-cat ${GH_WORK_DIR}/template/package.json | jq '.devDependencies |= .+{"@technote-space/register-grouped-format-type": "^'${VERSION}'"}' > ${GH_WORK_DIR}/template/TMP && mv -f ${GH_WORK_DIR}/template/TMP ${GH_WORK_DIR}/template/package.json
+VERSION=$(< ${TRAVIS_BUILD_DIR}/package.json jq -r .version)
+< ${GH_WORK_DIR}/template/package.json jq '.devDependencies |= .+{"@technote-space/register-grouped-format-type": "^'${VERSION}'"}' > ${GH_WORK_DIR}/template/TMP && mv -f ${GH_WORK_DIR}/template/TMP ${GH_WORK_DIR}/template/package.json
 
 cp -f ${current}/plugin.js ${GH_WORK_DIR}/
 cp -f ${current}/plugin.scss ${GH_WORK_DIR}/
